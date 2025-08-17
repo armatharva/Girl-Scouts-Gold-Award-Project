9dfd5782acc18dc976961ce9482d719a0c744f05
armatharva <atharvarmudu@gmail.com>
2025-08-17 14:05:25 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import EducationalComponenets from "./EducationalComponenets";
 import HygenieAdvice from "./HygenieAdvice";
 import Videodemo from "./Videodemo";
+import DoctorQA from "./DoctorQA";
+import Reproductivesystem from "./Reproductivesystem";
+import ProductComparison from "./ProductComparison";
 
 function App() {
   return (
@@ -9,13 +12,19 @@ function App() {
       <nav style={{ padding: "1rem", background: "#eee" }}>
         <Link to="/" style={{ marginRight: "1rem" }}>Education</Link>
         <Link to="/hygiene" style={{ marginRight: "1rem" }}>Hygiene Advice</Link>
-        <Link to="/video">Video Demo</Link>
+        <Link to="/video" style={{ marginRight: "1rem" }}>Video Demo</Link>
+        <Link to="/qa" style={{ marginRight: "1rem" }}>Doctor Q&A</Link>
+        <Link to="/reproductive" style={{ marginRight: "1rem" }}>Reproductive System</Link>
+        <Link to="/comparison">Product Comparison</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<EducationalComponenets />} />
         <Route path="/hygiene" element={<HygenieAdvice />} />
         <Route path="/video" element={<Videodemo />} />
+        <Route path="/qa" element={<DoctorQA />} />
+        <Route path="/reproductive" element={<Reproductivesystem />} />
+        <Route path="/comparison" element={<ProductComparison />} />
       </Routes>
     </Router>
   );
